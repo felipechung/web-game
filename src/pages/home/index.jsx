317e6d7a93aa22bee2ceaa355b1123294970c521
Felipe Chung <fchung.dev@gmail.com>
2023-09-08 17:09:19 -0300

    add extra input and buttons to homepage

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 
 export const Home = () => {
   const [name, setName] = useState("");
+  const [roomCode, setRoomCode] = useState("");
   return (
     <div className="border">
       <input
@@ -14,8 +15,21 @@ export const Home = () => {
         value={name}
         onChange={(event) => setName(event.target.value)}
       />
+      <input
+        type="text"
+        id="roomCode"
+        name="roomCode"
+        required
+        size="10"
+        value={roomCode}
+        onChange={(event) => setRoomCode(event.target.value)}
+      />
       <button type="button" onClick={() => console.log(name)}>
-        Enter
+        Entrar na sala
+      </button>
+
+      <button type="button" onClick={() => console.log(name)}>
+        Criar sala
       </button>
     </div>
   );
